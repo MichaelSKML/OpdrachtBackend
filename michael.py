d637cb5c2b63b511cc10351cb023064198c70279
MichaelSKML <MichaelSKML@gmail.com>
2024-04-08 15:45:12 +0200

    koppeling database

--- michael.py
+++ michael.py
@@ -1,3 +1,24 @@
+import mysql.connector
+
 def receptdetails(gid):
-  print("hier", gid)
-  return "dit is receptdetails"
+  
+  mydb = mysql.connector.connect(
+        host="yc2403allpurpose.mysql.database.azure.com",  #port erbij indien mac
+        user="yc2403admin",
+        password="abcd1234ABCD!@#$",
+        database="demopythondag"
+    )
+
+  mycursor = mydb.cursor()
+
+  mycursor.execute("SELECT * FROM recept WHERE id = " +str(gid))
+
+  myresult = mycursor.fetchall()
+  keys = [i[0] for i in mycursor.description]
+
+  data = [
+        dict(zip(keys, row)) for row in myresult
+    ]
+  return data
+
+
