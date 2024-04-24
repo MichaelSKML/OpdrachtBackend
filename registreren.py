98616074289315c24e18e84a6c40d51b89a91151
MichaelSKML <MichaelSKML@gmail.com>
2024-04-24 11:49:42 +0200

    mysql update

--- registreren.py
+++ registreren.py
@@ -12,12 +12,12 @@ CORS(app)
 # Database connectie
 load_dotenv()
 
-mydb = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
-)
+# mydb = mysql.connector.connect(
+#     host=os.getenv("DB_HOST"),
+#     user=os.getenv("DB_USER"),
+#     password=os.getenv("DB_PASSWORD"),
+#     database=os.getenv("DB_NAME")
+# )
 
 # Endpoint voor het registreren van een nieuwe gebruiker
 @app.route('/registreren', methods=['POST'])
