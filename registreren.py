294bf96d2460316938bbcf2ab0cf48204145b923
Maiordomo <d.partouns@gmail.com>
2024-04-25 08:58:51 +0200

    wijzigingen

--- registreren.py
+++ registreren.py
@@ -14,13 +14,6 @@ CORS(app)
 load_dotenv()
 mydb = algemenefuncties.verbindingdb()
 
-# mydb = mysql.connector.connect(
-#     host=os.getenv("DB_HOST"),
-#     user=os.getenv("DB_USER"),
-#     password=os.getenv("DB_PASSWORD"),
-#     database=os.getenv("DB_NAME")
-# )
-
 # Endpoint voor het registreren van een nieuwe gebruiker
 @app.route('/registreren', methods=['POST'])
 def registreren_function():
